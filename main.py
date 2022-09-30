3f55d477d9af3a7f8b58001d7b1a95bce5f09234
max <wema0930@hss-wi.de>
2022-09-30 10:02:09 +0200

    LLMN Webshop demo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, url_for, redirect
 from __init__ import create_app
 
 bp_main = Blueprint('main', __name__)
@@ -8,6 +8,31 @@ bp_main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+
+@bp_main.route('/engraves')
+def engraves():
+    all_engraves = ""
+
+    try:
+        f = open('engraves.txt', 'r')
+        f.seek(0)
+        all_engraves = f.readlines()
+        f.close()
+    except:
+        f = open('engraves.txt', 'x')
+        f.close()
+
+    return render_template('engraves.html', engraves=all_engraves)
+
+
+@bp_main.route('/engraves', methods=['POST'])
+def engraves_post():
+    engrave = "\n" + request.form.get('txt')
+    f = open('engraves.txt', 'a')
+    f.writelines(engrave)
+    f.close()
+
+    return redirect(url_for('main.engraves'))
 
 if __name__ == '__main__':
     create_app().run(debug=True, port=9876)
